4a5328808a3018ab2eb1bb4f4f5cb0f95ef6995c
Artem Priadchenko <yorgan.info@gmail.com>
2023-07-10 16:02:43 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+
+
+    internal class Program
+    {
+        static void MyTask1()
+        {
+            Console.WriteLine("MyTask1: запущен.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("+");
+            }
+            Console.WriteLine("MyTask1: завершен.");
+        }
+
+        static void MyTask2()
+        {
+
+
+            Console.WriteLine("MyTask2: запущен.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("-");
+            }
+            Console.WriteLine("MyTask2: завершен.");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Parallel.Invoke(MyTask1, MyTask2));
+
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("%");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] ints = new int[1_000_000];
+
+            Action<int> action = (i) =>
+            {
+                ints[i] = new Random().Next(1000);
+            };
+
+            Parallel.For(0, ints.Length, action);
+
+
+            var negatives = ints.AsParallel().AsOrdered().Where(element => (element % 2) != 0);
+
+
+            foreach (int element in negatives)
+                Console.Write(element + " ");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
